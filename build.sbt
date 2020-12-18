bf142b4c442e48015427b9205ff5f5e70559cf91
fabriciodelocco <38291350+fabriciodelocco@users.noreply.github.com>
2020-12-18 17:38:50 -0300

    initial commit

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "reportUraArmazenamento_v2"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
+// https://mvnrepository.com/artifact/commons-httpclient/commons-httpclient
+libraryDependencies += "commons-httpclient" % "commons-httpclient" % "3.1"
+// https://mvnrepository.com/artifact/org.elasticsearch/elasticsearch-hadoop
+libraryDependencies += "org.elasticsearch" % "elasticsearch-hadoop" % "7.10.0"
+// https://mvnrepository.com/artifact/org.elasticsearch/elasticsearch-spark-20
+libraryDependencies += "org.elasticsearch" %% "elasticsearch-spark-20" % "7.10.0"
+// https://mvnrepository.com/artifact/joda-time/joda-time
+libraryDependencies += "joda-time" % "joda-time" % "2.10.8"
---
+++ src/main/scala/reportUraArmazenamento_v2.scala
@@ -0,0 +1,55 @@
+import org.elasticsearch.spark._
+import org.apache.commons._
+import org.apache.http._
+import org.apache.http.client._
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object reportUraArmazenamento_v2 {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().appName("ReportUraArmazenamento").getOrCreate()
+    import org.joda.time.{DateTime, Instant, LocalDate}
+    import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+
+    val fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm")
+    DateTimeFormat.forPattern("yyyyMMdd")
+    val anteontem =  new DateTime().minusDays(2).withTimeAtStartOfDay().toString(DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:SS"))
+    val ontem =  new DateTime().minusDays(1).withTimeAtStartOfDay().toString(DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:SS"))
+    val hoje = new DateTime().withTimeAtStartOfDay().toString(DateTimeFormat.forPattern("yyyyMMdd"))
+
+    val param = "datahora > \"" + anteontem + "\" and datahora < \"" + ontem + "\" order by datahora asc"
+
+    val uraarmazenamento = spark.read
+      .format("org.elasticsearch.spark.sql")
+      .option("es.nodes", "srvelkcasprod01")
+      .option("es.port", "9200")
+      .option("es.net.ssl", "true")
+      .option("es.nodes.discovery", "false")
+      .option("es.net.http.auth.user", "elastic")
+      .option("es.net.http.auth.pass", "2BrR8mEn@Oss")
+      .option("es.net.ssl.cert.allow.self.signed", "true")
+      .load("ura_armazenamento/_doc")
+
+    uraarmazenamento.createOrReplaceTempView("uraarmazenamento")
+
+    val query = "select * from uraarmazenamento where " + param
+
+    val saida = spark.sql(query)
+
+    saida
+      .coalesce(1)
+      .write
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .save("/data/uraoutage/collect/ura_armazenamento/uraoutage_"+hoje+".csv")
+
+    spark.stop()
+
+  }
+}
